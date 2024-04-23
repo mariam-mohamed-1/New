00a0c6d702c851684f9a6d11ccdb91d2d3ac9503
mariam-mohamed-1 <128905232+mariam-mohamed-1@users.noreply.github.com>
2024-04-23 11:46:24 +0200

    update homepage

--- routes/main.js
+++ routes/main.js
@@ -9,6 +9,5 @@ router.get('/' ,control.getHome );
 router.get('/about' ,control.getAbout );
 router.get('/contact-us' ,control.getConnect );
 router.post('/',control.postForm)
-router.get('/stay',control.getStay)
 
 module.exports.Router=router;
